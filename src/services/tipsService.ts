762192934538a93e24231ef54f3e3a3496de1bac
Qassim Musa <kassimmusa322@gmail.com>
2026-04-10 15:27:26 +0300

    Update tipsService.ts

--- src/services/tipsService.ts
+++ src/services/tipsService.ts
@@ -1,6 +1,6 @@
 import apiClient from './apiClient';
 
-export type TipCategory = '2+' | '4+' | 'gg' | '10+' | 'vip';
+export type TipCategory = 'free'|'2+' | '4+' | 'gg' | '10+' | 'vip';
 export type JackpotType = 'midweek' | 'mega';
 export type DCLevel = 0 | 3 | 4 | 5 | 6 | 7 | 10 | 99;
 
